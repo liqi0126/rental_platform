9c77fa778937ad841dbb3cf471ee679408ddbed0
Qi Li <liqi17thu@gmail.com>
2020-09-09 01:01:09 +0800

    MyRegisterSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,31 @@
+from abc import ABC
+
 from rest_framework import serializers
 from equipment.serializers import EquipmentSerializer
 from .models import User
+from rest_auth.registration.serializers import RegisterSerializer
+
+
+class MyRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(write_only=True)
+    last_name = serializers.CharField(write_only=True)
+    is_superuser = serializers.BooleanField(default=False)
+    is_staff = serializers.BooleanField(default=False)
+
+    def get_cleaned_data(self):
+        is_superuser = self.validated_data.get('is_superuser', False)
+        print(type(is_superuser))
+
+        return {
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'email': self.validated_data.get('email', ''),
+            # 'is_superuser': self.validated_data.get('is_superuser', False),
+            # 'is_staff': self.validated_data.get('is_staff', False)
+            'is_superuser': True,
+            'is_staff': True
+        }
 
 
 class UserSerializer(serializers.ModelSerializer):
