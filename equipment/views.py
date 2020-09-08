e07ede46b7130f8dce079352baddf0b51e8107b6
“natedingyifeng” <dingyifengthu18@163.com>
2020-09-08 11:22:13 +0800

    new API: GET /api/v1/equipment # get all equipments

--- equipment/views.py
+++ equipment/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed
 
 from equipment.models import Equipment
 from user.models import User
@@ -8,23 +8,56 @@ from user.models import User
 
 def create_new_equipment(request):
 
-    if request.method != 'POST':
-        return JsonResponse({'error': 'require POST'})
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        description = request.POST.get('description', '')
 
-    name = request.POST.get('name', '')
-    address = request.POST.get('address', '')
-    email = request.POST.get('email', '')
-    phone = request.POST.get('phone', '')
-    description = request.POST.get('description', '')
+        owner_name = request.POST.get('owner', '')
+        try:
+            owner = User.objects.get(username=owner_name)
+        except:
+            return JsonResponse({'error': 'no such a user'})
 
-    owner_name = request.POST.get('owner', '')
-    try:
-        owner = User.objects.get(username=owner_name)
-    except:
-        return JsonResponse({'error': 'no such a user'})
+        equipment = Equipment(name=name, address=address, email=email, phone=phone, description=description, owner=owner)
 
-    equipment = Equipment(name=name, address=address, email=email, phone=phone, description=description, owner=owner)
+        equipment.save()
 
-    equipment.save()
+        return JsonResponse({'equipment': name})
 
-    return JsonResponse({'equipment': name})
+    else:
+        if request.method != 'GET':
+            return HttpResponseNotAllowed(['GET'])
+        else:
+            return get_all_equipments(request)
+
+
+def get_all_equipments(request):
+
+    equipment_list = Equipment.objects.all()
+    equipment_list_json = []
+    for item in equipment_list:
+        if item.status == 'REN':
+            equipment_list_json.append({
+                "name": item.name,
+                "owner": item.owner.username,
+                "address": item.address,
+                "email": item.email,
+                "phone": str(item.phone),
+                "status": item.status,
+                "lease_term_end": item.lease_term_end
+            })
+        else:
+            equipment_list_json.append({
+                "name": item.name,
+                "owner": item.owner.username,
+                "address": item.address,
+                "email": item.email,
+                "phone": str(item.phone),
+                "status": item.status,
+                "lease_term_end": "尚未租出"
+            })
+
+    return JsonResponse(equipment_list_json, safe=False)
