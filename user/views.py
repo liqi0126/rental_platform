d690dc816e2c7ff93d32295b3ef96883bf0c7fa1
Qi Li <liqi17thu@gmail.com>
2020-09-08 14:48:12 +0800

    minor

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password, check_password
-from user.models import User, UserSerializer
+from user.models import User
+from user.serializers import UserSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
