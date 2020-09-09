03f522e7c02ed7067d14c0c9315216296be607dc
Qi Li <liqi17thu@gmail.com>
2020-09-09 22:44:31 +0800

    change pagesize style

--- rental_platform/settings.py
+++ rental_platform/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter'
 
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
 
